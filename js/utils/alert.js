3fcad9c5fbddee1b0257dd3fc81e91511d247dc4
Bouyahi Mohamed <bouyahi.mohamed.1@gmail.com>
2025-08-25 17:16:00 +0200

    Refactor alert.js to correct variable naming and improve alert message handling

--- js/utils/alert.js
+++ js/utils/alert.js
@@ -1,15 +1,18 @@
 export function showAlert(message, type = "warning", closeauto = true) {
-  const alertDiv = document.getElementsByClassName("show-alert")[0];
-  if (!alertDiv) {
+  const alertContainer = document.getElementsByClassName("show-alert")[0];
+  if (!alertContainer) {
     console.error("No element with class 'show-alert' found.");
     return;
   }
+  let alertDiv = document.createElement("div");
   alertDiv.className = `alert alert-${type} alert-dismissible fade show zindex-toast`;
   alertDiv.setAttribute("role", "alert");
   alertDiv.innerHTML = `
     <strong>${message}</strong>
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
   `;
+ alertContainer.appendChild(alertDiv);
+
   if (closeauto) {
     setTimeout(() => {
       if (alertDiv.parentNode) {
